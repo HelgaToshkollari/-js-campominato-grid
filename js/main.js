54d58795214d70d7a9e41ef1f56d6f3b0ca68555
HelgaToshkollari <toshkollari.helga@gmail.com>
2022-11-09 17:51:52 +0100

    adds squares again

--- js/main.js
+++ js/main.js
@@ -5,11 +5,16 @@ const btnGenerate = document.querySelector(".btn-play");
 //bottone deve generare la griglia 
 btnGenerate.addEventListener("click", function(){
     console.log(this);
+    
+    squareContainer.innerHTML = "";
 
     for (let i = 1; i <= 100; i++){
         const newSquare = document.createElement("div");
+
+        newSquare.classList.add("grid-squares");
+        newSquare.style.width = "calc ( 100% / 10 )";
         newSquare.innerHTML = i;
-        newSquare.classList.add("squares");
+       
 
         newSquare.addEventListener("click", function(){
             console.log(this.innerHTML);
