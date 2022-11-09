b9c3a310c01d784805ce49ce0dc7d5a21b58e065
HelgaToshkollari <toshkollari.helga@gmail.com>
2022-11-09 21:40:58 +0100

    adds changes

--- js/main.js
+++ js/main.js
@@ -5,7 +5,8 @@ const btnGenerate = document.querySelector(".btn-play");
 //bottone deve generare la griglia 
 btnGenerate.addEventListener("click", function(){
     console.log(this);
-    const level = document.querySelector("difficoltà").value;
+    squareContainer.innerHTML = "";
+    const level = document.getElementById("difficoltà").value;
     if(level === "hard"){
        squareContainer.innerHTML = ""; 
 
@@ -13,7 +14,7 @@ btnGenerate.addEventListener("click", function(){
        
         const newSquare = document.createElement("div");
         newSquare.classList.add("grid-squares");
-        newSquare.style.flexBasis = 100 /10 + "%";
+        newSquare.style.flexBasis = 100 / 10 + "%";
         newSquare.innerHTML = i;
        
 
@@ -27,17 +28,54 @@ btnGenerate.addEventListener("click", function(){
         squareContainer.append(newSquare);
 
 
-        }
-
-    } else if {
-
-        
     }
-   
+    } else if (level === "medium") {
+        squareContainer.innerHTML = ""; 
 
-   
+        for (let i = 1; i <=81; i++){
+       
+        const newSquare = document.createElement("div");
+        newSquare.classList.add("medium-squares");
+        newSquare.style.flexBasis = 81 / 9 + "%";
+        newSquare.innerHTML = i;
+       
+
+        newSquare.addEventListener("click", function(){
+            console.log(this.innerHTML);
+            newSquare.classList.add("bgc-square");
+
+        })
 
 
+        squareContainer.append(newSquare);
+
+
+    }
+    } else if (level === "easy") {
+        squareContainer.innerHTML = ""; 
+
+        for (let i = 1; i <=49; i++){
+       
+        const newSquare = document.createElement("div");
+        newSquare.classList.add("easy-squares");
+        newSquare.style.flexBasis = 49 / 7 + "%";
+        newSquare.innerHTML = i;
+       
+
+        newSquare.addEventListener("click", function(){
+            console.log(this.innerHTML);
+            newSquare.classList.add("bgc-square");
+
+        })
+
+
+        squareContainer.append(newSquare);
+
+
+    }
+    }
+    
+    
 
 
 })
